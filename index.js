7ba0df1f106372fe39d22e7f9c7abee882cb1ff1
swapnil chavan <chavanswapnil822@gmail.com>
2023-10-05 15:59:38 +0530

    Addede Multiple datas for get data by weekly and by the day

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    
+
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
@@ -26,6 +26,16 @@ const connectDB = async () => {
 };
 
 const BookedSeat = require('./models/bookedseats');
+
+const {
+  MondaySeat,
+  TuesdaySeat,
+  WednesdaySeat,
+  ThursdaySeat,
+  FridaySeat,
+  SaturdaySeat,
+  SundaySeat,
+} = require('./models/bookedseats'); // Import your Mongoose models here
 
 // Twilio API credentials
 const accountSid = 'ACf495c3028b01961eb2fe87cc4a917bb2';
@@ -42,6 +52,211 @@ app.use(bodyParser.json());
 // Enable CORS to allow requests from your React frontend
 app.use(cors());
 
+
+// Book the monday seats
+app.post('/book-monday', async (req, res) => {
+  const { selectedSeats } = req.body;
+
+  try {
+    // Store the selected seats in the bookedSeats collection in your database
+    await MondaySeat.insertMany(selectedSeats.map((seatNumber) => ({ seatNumber })));
+
+    res.json({ success: true });
+  } catch (error) {
+    console.error('Failed to book seats:', error);
+    res.status(500).json({ success: false, error: 'Failed to book seats' });
+  }
+});
+
+// Book the Tuesday seats
+app.post('/book-tuesday', async (req, res) => {
+  const { selectedSeats } = req.body;
+
+  try {
+    // Store the selected seats in the bookedSeats collection in your database
+    await TuesdaySeat.insertMany(selectedSeats.map((seatNumber) => ({ seatNumber })));
+
+    res.json({ success: true });
+  } catch (error) {
+    console.error('Failed to book seats:', error);
+    res.status(500).json({ success: false, error: 'Failed to book seats' });
+  }
+});
+
+
+// Book the Wednesday seats
+app.post('/book-wednesday', async (req, res) => {
+  const { selectedSeats } = req.body;
+
+  try {
+    // Store the selected seats in the bookedSeats collection in your database
+    await WednesdaySeat.insertMany(selectedSeats.map((seatNumber) => ({ seatNumber })));
+
+    res.json({ success: true });
+  } catch (error) {
+    console.error('Failed to book seats:', error);
+    res.status(500).json({ success: false, error: 'Failed to book seats' });
+  }
+});
+
+
+// Book the Thursday seats
+app.post('/book-thursday', async (req, res) => {
+  const { selectedSeats } = req.body;
+
+  try {
+    // Store the selected seats in the bookedSeats collection in your database
+    await ThursdaySeat.insertMany(selectedSeats.map((seatNumber) => ({ seatNumber })));
+
+    res.json({ success: true });
+  } catch (error) {
+    console.error('Failed to book seats:', error);
+    res.status(500).json({ success: false, error: 'Failed to book seats' });
+  }
+});
+
+
+// Book the Friday seats
+app.post('/book-friday', async (req, res) => {
+  const { selectedSeats } = req.body;
+
+  try {
+    // Store the selected seats in the bookedSeats collection in your database
+    await FridaySeat.insertMany(selectedSeats.map((seatNumber) => ({ seatNumber })));
+
+    res.json({ success: true });
+  } catch (error) {
+    console.error('Failed to book seats:', error);
+    res.status(500).json({ success: false, error: 'Failed to book seats' });
+  }
+});
+
+
+// Book the saturday seats
+app.post('/book-saturday', async (req, res) => {
+  const { selectedSeats } = req.body;
+
+  try {
+    // Store the selected seats in the bookedSeats collection in your database
+    await SaturdaySeat.insertMany(selectedSeats.map((seatNumber) => ({ seatNumber })));
+
+    res.json({ success: true });
+  } catch (error) {
+    console.error('Failed to book seats:', error);
+    res.status(500).json({ success: false, error: 'Failed to book seats' });
+  }
+});
+
+
+// Book the Sunday seats
+app.post('/book-sunday', async (req, res) => {
+  const { selectedSeats } = req.body;
+
+  try {
+    // Store the selected seats in the bookedSeats collection in your database
+    await SundaySeat.insertMany(selectedSeats.map((seatNumber) => ({ seatNumber })));
+
+    res.json({ success: true });
+  } catch (error) {
+    console.error('Failed to book seats:', error);
+    res.status(500).json({ success: false, error: 'Failed to book seats' });
+  }
+});
+
+
+// Route to get the list of booked Monday seats from the database
+app.get('/booked-monday', async (req, res) => {
+  try {
+    // Query the database to get all booked seats
+    const bookedSeats = await MondaySeat.find({}, { seatNumber: 1, _id: 0 });
+    const bookedSeatNumbers = bookedSeats.map((seat) => seat.seatNumber);
+    res.json({ bookedSeats: bookedSeatNumbers });
+  } catch (error) {
+    console.error('Failed to fetch booked seats:', error);
+    res.status(500).json({ success: false, error: 'Failed to fetch booked seats' });
+  }
+});
+
+// Route to get the list of booked Tuesday seats from the database
+app.get('/booked-tuesday', async (req, res) => {
+  try {
+    // Query the database to get all booked seats
+    const bookedSeats = await TuesdaySeat.find({}, { seatNumber: 1, _id: 0 });
+    const bookedSeatNumbers = bookedSeats.map((seat) => seat.seatNumber);
+    res.json({ bookedSeats: bookedSeatNumbers });
+  } catch (error) {
+    console.error('Failed to fetch booked seats:', error);
+    res.status(500).json({ success: false, error: 'Failed to fetch booked seats' });
+  }
+});
+
+// Route to get the list of booked Wednesday seats from the database
+app.get('/booked-wednesday', async (req, res) => {
+  try {
+    // Query the database to get all booked seats
+    const bookedSeats = await WednesdaySeat.find({}, { seatNumber: 1, _id: 0 });
+    const bookedSeatNumbers = bookedSeats.map((seat) => seat.seatNumber);
+    res.json({ bookedSeats: bookedSeatNumbers });
+  } catch (error) {
+    console.error('Failed to fetch booked seats:', error);
+    res.status(500).json({ success: false, error: 'Failed to fetch booked seats' });
+  }
+});
+
+
+// Route to get the list of booked thurstday seats from the database
+app.get('/booked-thursday', async (req, res) => {
+  try {
+    // Query the database to get all booked seats
+    const bookedSeats = await ThursdaySeat.find({}, { seatNumber: 1, _id: 0 });
+    const bookedSeatNumbers = bookedSeats.map((seat) => seat.seatNumber);
+    res.json({ bookedSeats: bookedSeatNumbers });
+  } catch (error) {
+    console.error('Failed to fetch booked seats:', error);
+    res.status(500).json({ success: false, error: 'Failed to fetch booked seats' });
+  }
+});
+
+// Route to get the list of booked thurstday seats from the database
+app.get('/booked-friday', async (req, res) => {
+  try {
+    // Query the database to get all booked seats
+    const bookedSeats = await FridaySeat.find({}, { seatNumber: 1, _id: 0 });
+    const bookedSeatNumbers = bookedSeats.map((seat) => seat.seatNumber);
+    res.json({ bookedSeats: bookedSeatNumbers });
+  } catch (error) {
+    console.error('Failed to fetch booked seats:', error);
+    res.status(500).json({ success: false, error: 'Failed to fetch booked seats' });
+  }
+});
+
+// Route to get the list of booked saturday seats from the database
+app.get('/booked-saturday', async (req, res) => {
+  try {
+    // Query the database to get all booked seats
+    const bookedSeats = await SaturdaySeat.find({}, { seatNumber: 1, _id: 0 });
+    const bookedSeatNumbers = bookedSeats.map((seat) => seat.seatNumber);
+    res.json({ bookedSeats: bookedSeatNumbers });
+  } catch (error) {
+    console.error('Failed to fetch booked seats:', error);
+    res.status(500).json({ success: false, error: 'Failed to fetch booked seats' });
+  }
+});
+
+// Route to get the list of booked sunday seats from the database
+app.get('/booked-sunday', async (req, res) => {
+  try {
+    // Query the database to get all booked seats
+    const bookedSeats = await SundaySeat.find({}, { seatNumber: 1, _id: 0 });
+    const bookedSeatNumbers = bookedSeats.map((seat) => seat.seatNumber);
+    res.json({ bookedSeats: bookedSeatNumbers });
+  } catch (error) {
+    console.error('Failed to fetch booked seats:', error);
+    res.status(500).json({ success: false, error: 'Failed to fetch booked seats' });
+  }
+});
+
+// Book the seats
 app.post('/book-seats', async (req, res) => {
   const { selectedSeats } = req.body;
 
@@ -67,15 +282,6 @@ app.get('/booked-seats', async (req, res) => {
     console.error('Failed to fetch booked seats:', error);
     res.status(500).json({ success: false, error: 'Failed to fetch booked seats' });
   }
-});
-
-app.get('/welcome', (req, res) => {
-  res.send('Welcome to the application!');
-});
-
-
-app.get('/new', (req, res) => {
-  res.send('Welcome to the New Port!');
 });
 
 // Send SMS route
